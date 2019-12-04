e956ec118c78bf235cf94d6c1e744e55f179d90f
Perumal Mani <perumal.mani@agilysys.com>
2019-12-04 06:56:54 -0500

    Updated to call the stay profile-service internal api for checkprofile

--- CheckProfile/index.js
+++ CheckProfile/index.js
@@ -5,9 +5,10 @@ module.exports = async function (context, req) {
     context.log('profile_id=' + context.bindingData.profile_id);
     
     if (context.bindingData.profile_id) {
-        let fileurl = "https://rsg-webapp-tcc3.azurewebsites.net/documentation";
+        let url = "https://stay-blue.hospitalityrevolution.com/profile-service/v1/tenants/" + 
+        context.bindingData.tenantId + "/properties/" + context.bindingData.propertyId + "/guests/"+context.bindingData.profile_id;
         let response;
-        response = await broker.callHttpGet(fileurl, '', req.body);
+        response = await broker.callHttpGet(url, context.bindingData.headers.token, req.body);
 
         context.res = {
             // status: 200, /* Defaults to 200 */
--- Utilities/httpbroker.js
+++ Utilities/httpbroker.js
@@ -1,51 +1,29 @@
-const request = require("request");
+//const request = require("request");
 const util = require('util');
+const request = require('request-promise')
+
 const getRequestAsync = util.promisify(request.get);
 const postRequestAsync = util.promisify(request.post);
- 
+
 module.exports= {
- 
-callHttpGet: async function (uri, token, payload) {
+
+
+    callHttpGet: async function (uri, token, payload) {
+
         console.log('Calling http get ..');
         let response;
         try {
             response = await getRequestAsync(
                 {
-                    url: uri, 
-                    encoding: null, 
+                    url: uri,
+                    encoding: null,
                     headers: {
                         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36',
-                        'cache-control': 'max-age=0',
+                        'cache-control': 'max-age=0','Authorization':token,'X-Token':token
                     },
                     timeout:10000
                 });
-        } catch (error) {
-            // Any exceptions raised while processing error do not need to be handled like we had to when returning a new Promise. The exception will be properly caught by Azure Functions
-            console.log(error.toString());
-            // Re-throwing this because Azure functions automatically catches this exception and will mark your execution as "failed"
-            throw error;
-        }
-        return response;
-    },
- 
-    callHttpPost: async function  (uri,token, payload) {
-        console.log('Calling http post ..');
-        let response;
-        try {
-            response = await postRequestAsync(
-                {
-                    method: 'POST',
-                    url: uri, 
-                    json: payload,
-                    encoding: null, 
-                    headers: {
-                        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36',
-                        'cache-control': 'max-age=0',
-                        'Authorization':'Bearer ' + token,
-                        'Content-Type': 'application/json' 
-                    },
-                    timeout:10000
-                });
+
         } catch (error) {
             // Any exceptions raised while processing error do not need to be handled like we had to when returning a new Promise. The exception will be properly caught by Azure Functions
             console.log(error.toString());
@@ -55,5 +33,65 @@ callHttpGet: async function (uri, token, payload) {
         return response;
     },
 
- 
+
+    callHttpPost: async function  (uri,token, payload) {
+
+        console.log('Calling http post ..', payload);
+        let response;
+
+        try {
+
+            response = await postRequestAsync(
+
+                {
+
+                    method: 'POST',
+
+                    url: uri,
+
+                    json: payload,
+
+                    //encoding: null,
+
+                    headers: {
+
+                        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36',
+
+                        'cache-control': 'max-age=0',
+
+                        'Authorization':'Bearer ' + token,
+
+                        'Content-Type': 'application/json' ,
+                        'Accept': 'application/json'
+
+                    },
+
+                    timeout:10000
+
+                });
+
+        } catch (error) {
+
+            // Any exceptions raised while processing error do not need to be handled like we had to when returning a new Promise. The exception will be properly caught by Azure Functions
+
+            console.log(error.toString());
+
+            // Re-throwing this because Azure functions automatically catches this exception and will mark your execution as "failed"
+
+            throw error;
+
+        }
+
+        return response;
+
+
+
+
+
+
+
+
+
+    },
+
 };
